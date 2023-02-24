cdccf27496a15c19f277c67ca7dc680c505a7fa9
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-02-24 18:37:19 +0200

    it now takes a single html file, gets its content and writes it again

--- source.py
+++ source.py
@@ -1,15 +1,21 @@
 import os
+import codecs
+
+
+from bs4 import BeautifulSoup
 from googletrans import Translator
+from html.parser import HTMLParser
+
 
 script_path = os.getcwd()
 
 
 ###
-# open the file and returns its content
+# open the file and returns its content as a string
 ###
 def open_file(file_name: str):
-    folder_path = script_path + r"\test files" + f"\{file_name}"
-    file_handle = open(folder_path)
+    folder_path = script_path + r"\Targeted_Website" + f"\{file_name}"
+    file_handle = codecs.open(folder_path, 'r', "utf-8")
     file_content = file_handle.read()
     file_handle.close()
 
@@ -24,8 +30,8 @@ def translate_sentence(text: str):
 
 
 def write_file(text: str, file_name: str):
-    folder_path = script_path + r"\test files" + f"\{file_name}"
-    file_handle = open(folder_path, "w")
+    folder_path = script_path + r"\Translated_Website" + f"\{file_name}"
+    file_handle = codecs.open(folder_path, "w", "utf-8")
     file_handle.write(text)
     file_handle.close()
 
@@ -33,11 +39,13 @@ def write_file(text: str, file_name: str):
 ##################################################################################################
 # running sequence
 def main():
-    file_content = open_file("test.txt")
+    file_content = open_file("about.html")
 
-    new_translated_content = translate_sentence(file_content)
+    print(file_content)
 
-    write_file(new_translated_content, "translated.txt")
+    # new_translated_content = translate_sentence(file_content)
+
+    write_file(file_content, "translated.html")
 
 
 
